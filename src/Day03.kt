d6c1d497c60623164e757d7f02dc9d6d0ecee49b
korsik <iliaslog37@gmail.com>
2023-12-06 14:44:57 +0200

    Day03 start

---
+++ src/Day03.kt
@@ -0,0 +1,26 @@
+fun main() {
+
+    fun part1(input: List<String>): Int {
+        var result = 0
+        input.forEach { line ->
+        }
+        return result
+    }
+
+    fun part2(input: List<String>): Int {
+        var result = 0
+
+        input.forEach { line ->
+        }
+        return result
+    }
+
+    val testInput = readInput("test_input")
+    testInput.forEach { line ->
+
+    }
+
+    val input = readInput("input")
+    part1(input).println()
+    part2(input).println()
+}
