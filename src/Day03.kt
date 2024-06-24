dfaa1a3dd60fa89c7f33e524bd1071d7c0e43a5c
Ilias Logothetis <ilogothetis@icloud.com>
2024-06-24 16:56:58 +0300

    working

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,35 +1,71 @@
 fun main() {
 
-    data class Point(val x: Int, val y: Int)
-    data class Numbers(val value: Int, val adjacentPoints: Set<Point>, val row: Int)
-    data class Symbols(val symbol: Char, val point: Int, val row: Int)
+    data class Numbers(var value: Int, val location: List<Int>, val row: Int)
+    data class Symbols(val symbol: String, val point: Int, val row: Int)
 
 
     fun part1(input: List<String>): Int {
         var result = 0
-        var numbers = mutableListOf<Int>()
+        val numbers = mutableListOf<Numbers>()
+        val symbols = mutableListOf<Symbols>()
         input.forEach { line ->
-//            println(line)
+            val row = input.indexOf(line)
             Regex("[0-9]+").findAll(line)
                 .map(MatchResult::value)
                 .toList()
                 .map {
-                    it.toInt()
-                    numbers.add(it.toInt() )
-                    println(it.length)
-                    println(line.indexOf(it))
-//                    if(input.indexOf(line)-1 > 0)
-//                    if(input.indexOf(line)+1 < input.size)
-//                        println(input[input.indexOf(line)+1])
+                    numbers.add(
+                        Numbers(it.toInt(),
+                            ((line.indexOf(it) - 1)..(line.indexOf(it) + it.length)).toList(),
+                            row
+                            )
+                    )
                 }
             Regex("""[\p{P}\p{S}&&[^.]]+""").findAll(line)
                 .map(MatchResult::value)
                 .toList()
                 .map {
-                    println(it)
+                    symbols.add(
+                        Symbols(it, line.indexOf(it), row)
+                    )
                 }
         }
-        println(numbers)
+        println(numbers.size)
+        // Check for adjacent
+        var num = mutableListOf<Int>()
+        var sum = 0
+//        symbols.sortBy { it.row }
+        symbols.forEach { symbol ->
+            var check = false
+            numbers.forEachIndexed { index, number ->
+                println("The Line location Symbol ${symbol.point} -> Number ${number.location}")
+                println("The row position Symbol ${symbol.row} -> Number ${number.row}")
+                if(symbol.row == 2) {
+                    println("Yes")
+                }
+                    if ((symbol.point in number.location) and
+                        (symbol.row in (number.row - 1)..(number.row + 1))
+                    ) {
+                        sum += 1
+                        num.add(index)
+//                        println(number.value)
+                        result += number.value
+//                        number.value = 0
+                        check = true
+//                        return@symb
+                    }
+            }
+            if(check) {
+//                println("Checking ${num.size} numbers")
+                num.forEach {
+                    numbers.removeAt(it)
+                }
+                num.clear()
+            }
+        }
+
+        println("The numbers list size is ${numbers.size}")
+        println(sum)
         return result
     }
 
@@ -46,7 +82,20 @@ fun main() {
 
     }
 
-    val input = readInput("test_input")
+    // 1197 numbers and 730 symbols
+
+    // 143 numbers should be left out
+
+    //548033, 291365,
+
+    // 531932 with 1054
+
+//    233823
+//    392612
+
+    val input = readInput("input")
+
     part1(input).println()
+//    Day03(input).solvePart1()
     part2(input).println()
 }
